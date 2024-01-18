c3560520c91370c1b73411e9d6e98957a29ca211
Ochi-Shoichi5539 <dekamintv@gmail.com>
2024-01-18 22:26:04 +0900

    fix spec/hogehuga_mock_spec.rb

--- lib/hogehuga_mock.rb
+++ lib/hogehuga_mock.rb
@@ -2,8 +2,10 @@
 
 require_relative "hogehuga_mock/version"
 
+# HogehugaMock module is for testing purposes.
 module HogehugaMock
   class Error < StandardError; end
+
   # Your code goes here...
   def self.hello
     "Hello World!!"
